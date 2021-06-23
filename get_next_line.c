9cfdbefb80f268ec1f4e9d882eebd07bfb95c2a2
Jon Iglesias Seoane <joiglesi@student.42urduliz.com>
2021-06-23 15:45:18 +0200

    another day, another line

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: joiglesi <joiglesi@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/21 08:41:36 by joiglesi          #+#    #+#             */
-/*   Updated: 2021/06/22 12:30:32 by joiglesi         ###   ########.fr       */
+/*   Updated: 2021/06/23 15:43:59 by joiglesi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,38 +16,39 @@
 #include <stdlib.h>
 
 #ifndef BUFFER_SIZE
-# define BUFFER_SIZE = 32
+# define BUFFER_SIZE 32
 #endif
 
-typedef struct	s_buff {
+typedef struct s_buff {
 	int				fd;
-	char			**lines;
-	int				index;
+	char			*buff;
 	struct s_buff	*next;
 }					t_buff;
 
-size_t	ft_strlen(const char *s)
+size_t	ft_strlen(const char *s, char c)
 {
 	size_t	len;
 
+	if (!s)
+		return (0);
 	len = 0;
-	while (s[len])
+	while (s[len] != c)
 		len++;
 	return (len);
 }
 
-int	ft_strnchr(const char *s, char c)
-{
-	int	i;
+char	*ft_strchr(const char *s, int c)
+{	
+	size_t	i;
 
+	if (!s)
+		return (NULL);
 	i = 0;
-	while (s[i])
-	{
-		if (s[i] == c)
-			return (i);
+	while (s[i] != c && s[i])
 		i++;
-	}
-	return (i);
+	if (s[i] == c)
+		return ((char *)&s[i]);
+	return (NULL);
 }
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
@@ -57,78 +58,21 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	if (!s)
 		return (NULL);
-	if (start >= ft_strlen(s))
+	if (start >= ft_strlen(s, '\0'))
 		substr = (char *)malloc(sizeof(char));
 	else
 		substr = (char *)malloc(len * sizeof(char) + 1);
 	if (substr == NULL)
+	{
+		free((char *)s);
 		return (NULL);
+	}
 	i = 0;
-	while (i < len && start < ft_strlen(s))
+	while (i < len && start < ft_strlen(s, '\0'))
 		substr[i++] = s[start++];
 	substr[i] = '\0';
+	free((char *)s);
 	return (substr);
-}
-
-static int	ft_split_count(const char *s, char c)
-{
-	int	c_index;
-	int	count;
-	int	i;
-
-	count = 0;
-	i = ft_strlen(s);
-	while (i > 0)
-	{
-		c_index = ft_strnchr(s, c);
-		if (c_index > 0)
-			count++;
-		s += c_index + 1;
-		i -= (c_index + 1);
-	}
-	return (count);
-}
-
-static int	ft_null_case(char **split, int index)
-{
-	split[index + 1] = NULL;
-	if (!split[index])
-	{
-		while (*split)
-			free(*(split++));
-		free(split);
-		return (1);
-	}
-	return (0);
-}
-
-char	**ft_split(const char *s, char c)
-{
-	char	**split;
-	int		c_index;
-	int		split_index;
-	int		count;
-
-	if (!s)
-		return (NULL);
-	count = ft_split_count(s, c);
-	split = (char **)malloc(sizeof(char *) * (count + 1));
-	if (!split)
-		return (NULL);
-	*split = NULL;
-	split_index = 0;
-	while (split_index < count)
-	{
-		c_index = ft_strnchr(s, c);
-		if (c_index > 0)
-		{
-			split[split_index++] = ft_substr(s, 0, c_index);
-			if (ft_null_case(split, split_index - 1))
-				return (NULL);
-		}
-		s += c_index + 1;
-	}
-	return (split);
 }
 
 t_buff	*ft_fd_search(t_buff *lst, int fd)
@@ -164,27 +108,72 @@ t_buff	*ft_lstnew(int fd)
 	if (!new)
 		return (NULL);
 	new->fd = fd;
-	new->index = 0;
+	new->buff = NULL;
 	new->next = NULL;
 	return (new);
 }
 
-char	*ft_read_file(int fd)
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*jstr;
+	size_t	len;
+	size_t	i;
+	size_t	j;
+
+	len = ft_strlen(s1, '\0') + ft_strlen(s2, '\0');
+	jstr = (char *)malloc(sizeof(char) * (len + 1));
+	if (jstr == NULL)
+	{
+		free((char *)s1);
+		free((char *)s2);
+		return (NULL);
+	}
+	len = ft_strlen(s1, '\0');
+	i = -1;
+	while (++i < len)
+		jstr[i] = s1[i];
+	len += ft_strlen(s2, '\0');
+	j = 0;
+	while (i < len)
+		jstr[i++] = s2[j++];
+	jstr[i] = '\0';
+	free((char *)s1);
+	free((char *)s2);
+	return (jstr);
+}
+
+int	ft_fd_finish(t_buff *current, char *buff, char **line)
+{
+	*line = ft_strjoin(*line, ft_substr(buff, 0, ft_strlen(buff, '\n')));
+	if (!(*line))
+		return (-1);
+	free(buff);
+	free(current);
+	return (0);
+}
+
+char	*ft_read_line(int fd, int bufflen)
 {
 	char	*buff;
 	char	*tmp;
 	int		b_read;
-	int		b_total;
 
-	b_total = BUFFER_SIZE;
-	buff = (char *)malloc(sizeof(char) * b_total);
-	while (b_read)
+	if (bufflen)
+		buff = ft_strjoin(tmp, buff);
+	else
+		buff = NULL;
+	b_read = BUFFER_SIZE;
+	while (ft_strchr(buff, '\n') == NULL && b_read == BUFFER_SIZE)
 	{
+		tmp = buff;
+		buff = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+		if (!buff)
+			return (NULL);
 		b_read = read(fd, buff, BUFFER_SIZE);
-		b_total += b_read;
-		buff = (char *)malloc(sizeof(char) * b_total);
-		b_total += b_read;
+		buff[b_read] = '\0';
+		buff = ft_strjoin(tmp, buff);
 	}
+	return (buff);
 }
 
 int	get_next_line(int fd, char **line)
@@ -193,24 +182,44 @@ int	get_next_line(int fd, char **line)
 	t_buff			*current;
 	char			*buff;
 
+	*line = NULL;
+	buff = NULL;
 	if (fd == -1)
 		return (-1);
 	if (!ft_fd_search(buffers, fd))
-		if(ft_lstadd_front(&buffers, ft_lstnew(fd)))
-			return (-1);
-	current = ft_fd_search(buffers, fd);
-	if (current && current->lines && current->lines[current->index])
 	{
-		*line = current->lines[current->index];
-		free(current->lines[current->index]);
-		current->lines[current->index] = NULL;
-		return (1);
+		if (ft_lstadd_front(&buffers, ft_lstnew(fd)))
+			return (-1);
 	}
-	buff = ft_read_file(fd);
-	return (0);
+	current = ft_fd_search(buffers, fd);
+	if (current->buff)
+	{
+		if (ft_strchr(current->buff, '\n'))
+		{
+			*line = ft_substr(current->buff, 0, ft_strlen(buff, '\n'));
+			current->buff = ft_substr(ft_strchr(current->buff, '\n'), 0,
+					ft_strlen(ft_strchr(current->buff, '\n'), '\0'));
+			if (!(*line) || !current->buff)
+				return (-1);
+			return (1);
+		}
+		current->buff = NULL;
+	}
+	buff = ft_read_line(fd, ft_strlen(buff, '\0'));
+	if (!buff)
+		return (-1);
+	if (ft_strlen(buff, '\0') < BUFFER_SIZE)
+		return (ft_fd_finish(current, buff, line));
+	*line = ft_strjoin(*line, ft_substr(buff, 0, ft_strlen(buff, '\n')));
+	current->buff = ft_substr(ft_strchr(buff, '\n'), 0,
+			ft_strlen(ft_strchr(buff, '\n'), '\0'));
+	if (!(*line) || !current->buff)
+		return (-1);
+	return (1);
 }
 
 int	main(void)
 {
+	printf("Hello 42\n");
 	return (0);
 }
